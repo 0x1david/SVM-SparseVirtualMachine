4a44744b9c4b9ae30bbec14c39b51398f6ffdde2
David Bousi <bousi.david@gmail.com>
2025-09-25 16:25:40 +0200

    start line array

--- src/chunk.c
+++ src/chunk.c
@@ -29,3 +29,39 @@ int addConst(Chunk *chunk, Value value) {
   writeValueArray(&chunk->constants, value);
   return chunk->constants.length - 1;
 }
+
+IMPLEMENT_ARRAY_FUNCTIONS(int, LineStart)
+
+int getLine(LineStartArray *arr, int pos) {
+  int start = 0;
+  int end = arr->length - 1;
+  if (pos < 0 || arr->length == 0) {
+    return -1;
+  }
+
+  if (arr->length == 1) {
+    return (pos >= arr->values[0]) ? 0 : -1;
+  }
+
+  while (start <= end) {
+    int middle = (start + end) / 2;
+    int middle_el = arr->values[middle];
+
+    if (pos < middle_el) {
+      if (middle > 0 && pos >= arr->values[middle - 1]) {
+        return middle - 1;
+      }
+
+      end = middle - 1;
+      continue;
+    }
+
+    else {
+      if (middle == arr->length - 1 || pos < arr->values[middle + 1]) {
+        return middle;
+      }
+      start = middle + 1;
+    }
+  }
+  return -1;
+}
--- src/chunk.h
+++ src/chunk.h
@@ -2,6 +2,7 @@
 #define svm_chunk_h
 
 #include "common.h"
+#include "memory.h"
 #include "value.h"
 
 typedef enum { OP_RETURN, OP_CONSTANT } OpCode;
@@ -17,5 +18,15 @@ void initChunk(Chunk *chunk);
 void writeChunk(Chunk *chunk, uint8_t byte);
 void freeChunk(Chunk *chunk);
 int addConst(Chunk *chunk, Value value);
+
+typedef struct {
+  int length;
+  int capacity;
+  int *values;
+} LineStartArray;
+
+DECLARE_ARRAY_FUNCTIONS(int, LineStart);
+
+int getLine(LineStartArray *arr, int pos);
 
 #endif
