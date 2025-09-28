d6ae383a71e3826988804e20f5d24fade30e1def
David Bousi <bousi.david@gmail.com>
2025-09-28 19:56:38 +0200

    cli

--- src/main.c
+++ src/main.c
@@ -1,37 +1,79 @@
 #include "chunk.h"
 #include "debug.h"
 #include "vm.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+static void repl(VM *vm) {
+  char line[1024];
+  for (;;) {
+    printf("> ");
+    if (!fgets(line, sizeof(line), stdin)) {
+      printf("\n");
+      break;
+    }
+    interpret(&vm, line);
+  }
+}
+
+static char *readFile(const char *path) {
+  FILE *file = fopen(path, "rb");
+  if (file == NULL) {
+    fprintf(stderr, "Could not open file \"%s\".\n", path);
+    exit(74);
+  }
+
+  fseek(file, 0L, SEEK_END);
+  size_t fileSize = ftell(file);
+  rewind(file);
+
+  char *buf = (char *)malloc(fileSize + 1);
+  if (buf == NULL) {
+    fprintf(stderr, "Not enough memory to read \"%s\".\n", path);
+    exit(74);
+  }
+
+  size_t bytesRead = fread(buf, sizeof(char), fileSize, file);
+  if (bytesRead < fileSize) {
+    fprintf(stderr, "Could not read file \"%s\".\n", path);
+    exit(74);
+  }
+  buf[bytesRead] = '\0';
+
+  fclose(file);
+  return buf;
+}
+
+static void runFile(VM *vm, const char *path) {
+  char *src = readFile(path);
+  InterpretResult res = interpret(vm, src);
+  free(src);
+
+  switch (res) {
+  case INTERPRET_COMPILE_ERROR:
+    exit(65);
+    break;
+  case INTERPRET_RUNTIME_ERROR:
+    exit(70);
+    break;
+  case INTERPRET_OK:
+    break;
+  }
+}
 
 int main(int argc, const char *argv[]) {
   VM vm;
   initVM(&vm);
-  Chunk ch;
-  initChunk(&ch);
-
-  // Test: 1.2 + 3.4 - 2.0 (should equal 2.6)
-  writeConst(&ch, 1.2, 123);
-  writeConst(&ch, 3.4, 123);
-  writeChunk(&ch, OP_ADD, 123, 3);
-
-  writeConst(&ch, 2.0, 123);
-  writeChunk(&ch, OP_SUBTRACT, 123, 3);
-
-  // Test: negate the result (should equal -2.6)
-  writeChunk(&ch, OP_NEGATE, 123, 3);
-
-  // Test: multiply by 2.0 (should equal -5.2)
-  writeConst(&ch, 2.0, 123);
-  writeChunk(&ch, OP_MULTIPLY, 123, 3);
-
-  // Test: divide by 4.0 (should equal -1.3)
-  writeConst(&ch, 4.0, 123);
-  writeChunk(&ch, OP_DIVIDE, 123, 3);
-
-  writeChunk(&ch, OP_RETURN, 123, 3);
-
-  disassembleChunk(&ch, "test chunk");
-  interpret(&vm, &ch);
+  switch (argc) {
+  case 1:
+    repl(&vm);
+    break;
+  case 2:
+    runFile(&vm, argv[1]);
+    break;
+  default:
+    fprintf(stderr, "Usage: clox [path]\n");
+  }
   closeVM(&vm);
-  freeChunk(&ch);
   return 0;
 }
